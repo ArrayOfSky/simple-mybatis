8fd8fdc8d2df0d3f0696dfe69c997cac07a714ec
Gaoyifeng <104308413+ArrayOfSky@users.noreply.github.com>
2024-04-13 14:13:02 +0800

    init

---
+++ src/main/java/com/arrayofsky/arrayofskymybatissimple/ArrayofskyMybatisSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.arrayofsky.arrayofskymybatissimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArrayofskyMybatisSimpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArrayofskyMybatisSimpleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arrayofsky/arrayofskymybatissimple/ArrayofskyMybatisSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arrayofsky.arrayofskymybatissimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArrayofskyMybatisSimpleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
